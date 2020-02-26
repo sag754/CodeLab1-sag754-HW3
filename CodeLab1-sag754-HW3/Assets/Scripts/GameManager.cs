45ff284e12321860ed7d9af5abec7229cb0bb6e6
sag754 <56234238+sag754@users.noreply.github.com>
2020-02-26 17:45:42 -0500

    Added previous project to expand upon for this assignment.

---
+++ CodeLab1-sag754-HW3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Text infoText;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        infoText = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        infoText.text = "Coins x " + PlayerController.instance.coins;
+    }
+}
